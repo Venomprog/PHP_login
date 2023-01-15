5aef9164ab504cba50d1b15d64589265e06a8566
Gleb <glebshaevvv@mail.ru>
2023-01-15 23:52:02 +0300

    comments

--- vendor/signin.php
+++ vendor/signin.php
@@ -4,11 +4,7 @@
 
     $login = $_POST['login'];
     $password = md5($_POST['password']);
-    // if {
-    //     // Сравнить логин и пароль из другого файла, если всё ок, то переадресация под админом
-    // } else {
-        
-    // }
+    
 
     if ($login == "admin"){
         
@@ -33,21 +29,4 @@
             header('Location: ../index.php');
         }
     }
-    // $check_user = mysqli_query($connect, "SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password' ");
-
-
-
-
-    // if (mysqli_num_rows($check_user) > 0){
-    //     $user = mysqli_fetch_assoc($check_user);
-    //     $_SESSION['user'] = [
-    //         "id" => $user['id'],
-    //         "full_name" => $user['full_name'],
-    //         "email" => $user['email']
-    //     ];
-    //     header('Location: ../profile.php');
-
-    // } else {
-    //     $_SESSION['message'] = 'Неверный логин или пароль';
-    //     header('Location: ../index.php');
-    // }
+    
